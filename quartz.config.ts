adb7bb6e125f10267f36f375488bec891d283f8d
Saekwang Nam <saekwang.nam@gmail.com>
2025-03-10 02:57:19 +0900

    Quartz sync: Mar 10, 2025, 2:57 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: `https://${cfg.baseUrl}/static/rottoda_logo.png`,
+    pageTitle: "Rottoda@KNU",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -8,7 +8,10 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const baseDir = pathToRoot(fileData.slug!)
   return (
     <h2 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>{title}</a>
+      <a href={baseDir}>
+        {/* {title}  // 주석 처리하여 텍스트 숨김 */}
+        <img src="https://github.com/s-nam/rottoda/blob/v4/quartz/static/rottoda_logo.png?raw=true" alt="Rottoda Logo" class="title-logo" />
+      </a>
     </h2>
   )
 }
@@ -17,6 +20,10 @@ PageTitle.css = `
 .page-title {
   font-size: 1.75rem;
   margin: 0;
+}
+.title-logo {
+  height: 100px;
+  vertical-align: middle;
 }
 `
 
